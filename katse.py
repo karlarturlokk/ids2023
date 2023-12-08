bec3771a507943630bd813c1bf1399c54345e3a0
guvystunad <126552990+guvystunad@users.noreply.github.com>
2023-12-08 11:45:25 +0200

    Create katse.py

---
+++ katse.py
@@ -0,0 +1,16 @@
+import tabula
+import pandas as pd
+#from pandasai import*
+from pandasai import SmartDataframe
+from pandasai.llm import OpenAI
+llm = OpenAI(api_token='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')
+
+
+dfs = tabula.read_pdf("NOTE_Parnu_OU-aruanne_2022.pdf", stream=True, pages='all')
+#print(len(dfs))
+#print(dfs[0])
+
+df = SmartDataframe(dfs[0], config={"llm": llm})
+vaste = df.chat('mis on firma aastakasumid ja vahemikud, järjesta aastate järgi kasvavalt')
+
+vaste.to_csv('tabel.csv')
